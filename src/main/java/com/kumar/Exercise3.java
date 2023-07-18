8f8a6652d45d7c90f3da02d47b74622448e9303a
rkumar <rakesh4vi@gmail.com>
2023-07-18 22:18:52 +0530

    Flux to Stream conversion.

--- src/main/java/com/kumar/Exercise3.java
+++ src/main/java/com/kumar/Exercise3.java
@@ -1,8 +1,19 @@
 package com.kumar;
 
+import java.util.List;
+
 /**
  * @author RakeshKumar created on 17/07/23
  */
 
 public class Exercise3 {
+    public static void main(String[] args) {
+        // Use ReactiveSources.integerFlux
+        //Get all numbers in the ReactiveSources.integerFlux stream into a list and list and size
+
+       // ReactiveSources.integerFlux().toStream().forEach(System.out::println);
+
+        List<Integer> integerList = ReactiveSources.integerFlux().toStream().toList();
+        System.out.println("Flux data ::"+integerList + "  Of size "+integerList.size());
+    }
 }
